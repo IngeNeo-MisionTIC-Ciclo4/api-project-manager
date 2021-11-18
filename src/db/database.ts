21529aff8ffd0c2ea4132f82927a153c01bd9334
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-11-17 23:34:05 -0500

    Iniciamos el modelado de la DB

---
+++ src/db/database.ts
@@ -0,0 +1,18 @@
+import { connect } from 'mongoose';
+import dotenv from 'dotenv';
+
+//Preparamos para poder usar variables de entorno
+dotenv.config({ path: './.env' });
+
+const conectarBD = async () => {
+	return await connect(process.env.DATABASE_URL)
+		.then(() => {
+			console.log('Conexion exitosa a la base de datos');
+		})
+		.catch((e) => {
+			console.error('Error conectando a la base de datos', e);
+		});
+};
+
+//Exportamos la funcion creada
+export { conectarBD };
