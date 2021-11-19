29de97591a0860d983256fa709c23ade9b20b0b5
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-11-18 22:59:27 -0500

    Organizacion codigo

--- index.js.js
+++ index.js.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import { conectarBD } from './src/db/database.js';
-
+import { ModeloUsuario } from './src/models/usuario/usuario.js';
 
 //Nos permite usar los archivos .env para tener las rutas en otro lado no visibles en el repo
 dotenv.config({ path: './.env' });
@@ -18,7 +18,6 @@ app.use(express.json());
 
 //Es el Middleware para permitir peticiones desde cualqueir ubicación
 app.use(cors());
-
 
 //Nos conectamos al base datos
 const main = async () => {
