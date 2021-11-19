eaf4cfb1bc71bfcc88ae46986841cf447a162481
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-11-18 23:42:44 -0500

    Comandos de creación

--- src/db/database.js
+++ src/db/database.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 //Preparamos para poder usar variables de entorno
 dotenv.config({ path: './.env' });
 
+//Definimos la función de conexión con la base de datos
 const conectarBD = async () => {
 	return await mongoose.connect(process.env.DATABASE_URL)
 		.then(() => {
--- src/models/usuario/usuario.js
+++ src/models/usuario/usuario.js
@@ -1,44 +1,43 @@
-
 import Mongoose from 'mongoose';
 
-const {Schema, model} = Mongoose;
+const { Schema, model } = Mongoose;
 
-const UsuarioSchema = new Schema  ({
-    cedula: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    nombres: {
-        type: String,
-        required: true,
-    },
-    apellidos: {
-        type: String,
-        required: true,
-    },
-    correo: {
-        type: String,
-        required: true,
-        unique: true,
-        validate: {
-            validator: (email) => {
-                return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
-            },
-            message: 'El formato del Email es incorrecto.',
-        },
-    },
-    tusuario: {
-        type: String,
-        enum: ['Estudiante', 'Lider', 'Administrador'],
-        },
-    estado: {
-        type: String,
-        enum: ['Pendiente', 'Autorizado', 'No_Autorizado'],
-        default: 'Pendiente',
-        },
+const UsuarioSchema = new Schema({
+	cedula: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	nombres: {
+		type: String,
+		required: true,
+	},
+	apellidos: {
+		type: String,
+		required: true,
+	},
+	correo: {
+		type: String,
+		required: true,
+		unique: true,
+		validate: {
+			validator: (email) => {
+				return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+			},
+			message: 'El formato del Email es incorrecto.',
+		},
+	},
+	tusuario: {
+		type: String,
+		enum: ['Estudiante', 'Lider', 'Administrador'],
+	},
+	estado: {
+		type: String,
+		enum: ['Pendiente', 'Autorizado', 'No_Autorizado'],
+		default: 'Pendiente',
+	},
 });
 
 const ModeloUsuario = model('Usuario', UsuarioSchema);
 
-export {ModeloUsuario};
+export { ModeloUsuario };
