c1a925963be8e0fca713386204b5c83488baea4a
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-11-30 11:35:09 -0500

    Creación y generación de token

---
+++ src/utils/token.js
@@ -0,0 +1,28 @@
+import jwt from 'jsonwebtoken';
+
+const validarToken = (token) => {
+	if (token) {
+		const verification = jwt.verify(token, 'secret', (err, data) => {
+			if (data) {
+				return {
+					data: data,
+				};
+			}
+			if (err) {
+				return {
+					error: err,
+				};
+			}
+		});
+		console.log(verification, token);
+		return verification;
+	}
+};
+
+const generarToken = (payload) => {
+	return jwt.sign(payload, 'secret', {
+		expiresIn: '24h',
+	});
+};
+
+export { generarToken, validarToken };
