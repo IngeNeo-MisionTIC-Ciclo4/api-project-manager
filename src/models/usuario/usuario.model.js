96132925e2aa1d04bfb31080d4029328786be8fc
LeidyGalla <papeta23@gmail.com>
2021-11-29 22:12:29 -0500

    RModificación de modelos y tipos y resolvers de usuarios

--- src/models/usuario/usuario.model.js
+++ src/models/usuario/usuario.model.js
@@ -2,41 +2,63 @@ import Mongoose from 'mongoose';
 
 const { Schema, model } = Mongoose;
 
-const UsuarioSchema = new Schema({
-	cedula: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	nombres: {
-		type: String,
-		required: true,
-	},
-	apellidos: {
-		type: String,
-		required: true,
-	},
-	correo: {
-		type: String,
-		required: true,
-		unique: true,
-		validate: {
-			validator: (email) => {
-				return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+const UsuarioSchema = new Schema
+(
+	{
+		cedula: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		nombres: {
+			type: String,
+			required: true,
+		},
+		apellidos: {
+			type: String,
+			required: true,
+		},
+		correo: {
+			type: String,
+			required: true,
+			unique: true,
+			validate: {
+				validator: (email) => {
+					return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email);
+				},
+				message: 'El formato del Email es incorrecto.',
 			},
-			message: 'El formato del Email es incorrecto.',
+		},
+		tusuario: {
+			type: String,
+			enum: ['Estudiante', 'Lider', 'Administrador'],
+		},
+		estado: {
+			type: String,
+			enum: ['Pendiente', 'Autorizado', 'No_Autorizado'],
+			default: 'Pendiente',
 		},
 	},
-	tusuario: {
-		type: String,
-		enum: ['Estudiante', 'Lider', 'Administrador'],
-	},
-	estado: {
-		type: String,
-		enum: ['Pendiente', 'Autorizado', 'No_Autorizado'],
-		default: 'Pendiente',
-	},
-});
+	{ versionKey: false }
+	);
+
+	UsuarioSchema.virtual('proyectosLiderados', {
+		ref: 'Proyecto',
+		localField: '_id',
+		foreignField: 'lider',
+	});
+
+	UsuarioSchema.virtual('avancesCreados', {
+		ref: 'Avance',
+		localField: '_id',
+		foreignField: 'estudiante',
+	});
+
+	UsuarioSchema.virtual('inscripciones', {
+		ref: 'Inscripcion',
+		localField: '_id',
+		foreignField: 'estudiante',
+	});
 
 const ModeloUsuario = model('Usuario', UsuarioSchema);
 
