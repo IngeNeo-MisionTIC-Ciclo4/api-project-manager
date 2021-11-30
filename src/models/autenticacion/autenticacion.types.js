de15cc3a35959943849d0d9e968f63bc96c9c6b7
Roger Valencia <roger.valencia.garcia@hotmail.com>
2021-11-30 11:31:51 -0500

    Creación funcionalidad de autenticación

---
+++ src/models/autenticacion/autenticacion.types.js
@@ -0,0 +1,27 @@
+import { gql } from 'apollo-server-express';
+
+const tiposAutenticacion = gql`
+
+	type Token {
+		token: String
+		error: String
+	}
+
+	type Mutation {
+		registro(
+			cedula: String
+			nombres: String
+			apellidos: String
+			correo: String!
+			password: String!
+			tusuario: Enum_Tusuario
+			estado: Enum_EstadoUsuario
+		): Token!
+
+		login(correo: String!, password: String!): Token
+
+		refreshToken: Token
+	}
+`;
+
+export { tiposAutenticacion };
